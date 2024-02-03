fa2b6ae764d6ae6b658c145b55de4ed01dc67183
mosesbenjamin <mavewreek@gmail.com>
2024-02-03 00:28:00 -0600

    pokeapi

---
+++ command_map.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandMapf(cfg *config) error {
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.previousLocationsURL = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.previousLocationsURL == nil {
+		return errors.New("you are on the first page")
+	}
+
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.previousLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.previousLocationsURL = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
